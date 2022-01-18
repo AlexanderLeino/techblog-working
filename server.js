1153ae08797198d6d669610aa9e83b48c96ebf69
Alexander Leino <Alexander.Leino@yahoo.com>
2022-01-18 16:27:44 -0500

    added more to server.js

--- server.js
+++ server.js
@@ -1,27 +1,39 @@
-require('dotenv').config()
-const sequelize = require('./config/connection')
-const express = require('express')
-const exphbs = require('express-handlebars')
-const controllers = require('./controllers')
+const express = require('express');
+const routes = require('./controllers');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+
+const app = express();
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 
-const PORT = 3001 || process.env.PORT
-const app = express()
+const PORT = process.env.PORT || 3001;
 
-app.engine('handlebars', exphbs())
-app.set('view engine', 'handlebars')
+const hbs = exphbs.create();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(express.static('public'))
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
-app.use(controllers)
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-    console.log(`Sequelize connected!`)
-    
-})
-// html routes
-
-
-app.listen(PORT, () => console.log(`Go to http://localhost:${PORT}`))
+    app.listen(PORT, () => console.log('Now listening on ${PORT}'));
+  });
