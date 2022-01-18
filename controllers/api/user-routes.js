62cda459e305b019a3087da847614a9119b0208c
Alexander Leino <Alexander.Leino@yahoo.com>
2022-01-18 18:32:05 -0500

    everything works

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -90,7 +90,7 @@ router.post('/login', async (req, res) => {
 
 // Logout
 router.post('/logout', (req, res) => {
-  
+ 
   if (req.session.loggedIn) {
     req.session.destroy(() => {
       res.status(204).end();
