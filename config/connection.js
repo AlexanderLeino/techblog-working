f7e7d0c6afd6e198e39138fa12abca55dcf91de4
Alexander Leino <Alexander.Leino@yahoo.com>
2022-01-18 16:31:07 -0500

    connection changes

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ const Sequelize = require('sequelize');
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-      host: '127.0.0.1',
+      host: 'localhost',
       dialect: 'mysql',
       port: 3306,
       dialectOptions: {
